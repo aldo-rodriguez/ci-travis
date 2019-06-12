e94ce526c04da2f663fdf32476829d378bbfe313
Aldo <aldo.rodriguez@autentia.com>
2019-06-12 15:31:19 +0200

    Init commit

---
+++ src/main/java/com/mylab/ci/travis/HelloController.java
@@ -0,0 +1,14 @@
+package com.mylab.ci.travis;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+    
+}
